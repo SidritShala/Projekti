aea358eb5ec36da1e7be4b9ab4f2b8125f4b17a4
Drinikb <kabashidrini3@gmail.com>
2025-02-02 03:19:21 +0100

    login controller added

---
+++ LoginController.php
@@ -0,0 +1,49 @@
+
+<?php
+require_once 'DBUser.php';
+
+class LoginController
+{
+    private $errorMessage;
+
+    public function __construct()
+    {
+        $this->errorMessage = '';
+    }
+
+    public function handleLogin()
+    {
+        if (isset($_POST['Login'])) {
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+
+            if (empty($email) || empty($password)) {
+                $this->errorMessage = "Please fill in all the fields.";
+                return;
+            }
+
+            $userDBHandler = new DBUser();
+            $user = $userDBHandler->getUserEmailPass($email, $password);
+
+            if (empty($user)) {
+                $this->errorMessage = "Email or Password is invalid.";
+            } else {
+                session_start();
+                if ($user['role'] == "user") {
+                    $_SESSION['useremail'] = $email;
+                    header("Location: index.php");
+                } else {
+                    $_SESSION['adminemail'] = $email;
+                    header("Location: dashboard.php");
+                }
+                exit;
+            }
+        }
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+}
+?>
